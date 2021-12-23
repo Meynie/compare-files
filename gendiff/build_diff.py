544e3efc8af175a9f7997b0eb62329e3ccb7cbc7
Meynie <ol.nevskaya@mail.ru>
2021-12-23 14:44:40 +0300

    fix

--- gendiff/build_diff.py
+++ gendiff/build_diff.py
@@ -1,24 +1,29 @@
 import json
 
 
+def read_file(file):
+    res = json.load(open(file))
+    return res
+
+
 def generate_diff(file1, file2):
     """Find differences in files."""
-    file1 = json.load(open(file1))
-    file2 = json.load(open(file2))
-    common = sorted(file1.keys() & file2.keys())
-    before = sorted(file1.keys() - file2.keys())
-    after = sorted(file2.keys() - file1.keys())
+    before_dict = read_file(file1)
+    after_dict = read_file(file2)
+    common = sorted(before_dict.keys() & after_dict.keys())
+    before = sorted(before_dict.keys() - after_dict.keys())
+    after = sorted(after_dict.keys() - before_dict.keys())
     diff = ['{']
-    for key in common:
-        if file1[key] == file2[key]:
-            diff.append('   ' + key + str(file1[key]))
-        else:
-            diff.append(' - ' + key + ': ' + str(file1[key]))
-            diff.append(' + ' + key + ': ' + str(file2[key]))
     for key in before:
-        diff.append(' - ' + key + ': ' + str(file1[key]))
+        diff.append(' - ' + key + ': ' + str(before_dict[key]))
+    for key in common:
+        if before_dict[key] == after_dict[key]:
+            diff.append('   ' + key + str(before_dict[key]))
+        else:
+            diff.append(' - ' + key + ': ' + str(before_dict[key]))
+            diff.append(' + ' + key + ': ' + str(after_dict[key]))
     for key in after:
-        diff.append(' + ' + key + ' ' + str(file2[key]))
+        diff.append(' + ' + key + ' ' + str(after_dict[key]))
     diff.append('}')
     result_diff = ''
     for elem in diff:
