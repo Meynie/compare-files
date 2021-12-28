d8fbd53a7fb1e115383d1b52cede766fa70de93c
Meynie <ol.nevskaya@mail.ru>
2021-12-28 11:32:53 +0300

    refactoring

--- gendiff/build_diff.py
+++ gendiff/build_diff.py
@@ -1,15 +1,19 @@
-import json
+import os
+
+from gendiff.parser_input import parser
 
 
-def read_file(file):
-    res = json.load(open(file))
-    return res
+def read_file(file_name):
+    """Read file and return dictionary."""
+    with open(file_name, 'r', encoding='utf-8') as file_object:
+        file_type = os.path.splitext(file_name)[-1]
+        file_data = file_object.read()
+        return parser(file_data, file_type)
 
 
 def generate_diff(file1, file2):
     """Find differences in files."""
-    before_dict = read_file(file1)
-    after_dict = read_file(file2)
+    before_dict, after_dict = read_file(file1), read_file(file2)
     common = sorted(before_dict.keys() & after_dict.keys())
     before = sorted(before_dict.keys() - after_dict.keys())
     after = sorted(after_dict.keys() - before_dict.keys())
