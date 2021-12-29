a4e0bcca26c0b44388d56843302f4640df019764
Meynie <ol.nevskaya@mail.ru>
2021-12-29 16:14:38 +0300

    adding tests

--- tests/test_json_diff.py
+++ tests/test_json_diff.py
@@ -2,12 +2,66 @@
 
 """JSON files diff test."""
 
-from gendiff.build_diff import generate_diff
-from tests.expected import DIFFERENCE_STRING
+from gendiff.generate import generate_diff
+from tests.expected import (
+    SIMPLE_DIFF_STRING,
+    COMPLEX_DIFF_STRING,
+    SIMPLE_DIFF_JSON,
+    COMPLEX_DIFF_JSON,
+    SIMPLE_DIFF_PLAIN,
+    COMPLEX_DIFF_PLAIN,
+)
+
+PATH_AFTER_SIMPLE = 'tests/fixture/simple_after.json'
+PATH_BEFORE_SIMPLE = 'tests/fixture/simple_before.json'
+
+PATH_AFTER_COMPLEX = 'tests/fixture/complex_after.json'
+PATH_BEFORE_COMPLEX = 'tests/fixture/complex_before.json'
 
 
-def test_generate_diff():
-    """Test generate_diff."""
-    actual = generate_diff('./tests/fixtures/before.json',
-                           './tests/fixtures/after.json')
-    assert actual == DIFFERENCE_STRING
+def test_simple_string_diff():
+    difference = generate_diff(
+        PATH_BEFORE_SIMPLE,
+        PATH_AFTER_SIMPLE,
+        'string')
+    assert difference == SIMPLE_DIFF_STRING
+
+
+def test_complex_string_diff():
+    difference = generate_diff(
+        PATH_BEFORE_COMPLEX,
+        PATH_AFTER_COMPLEX,
+        'string')
+    assert difference == COMPLEX_DIFF_STRING
+
+
+def test_simple_json_diff():
+    difference = generate_diff(
+        PATH_BEFORE_SIMPLE,
+        PATH_AFTER_SIMPLE,
+        'json')
+    assert difference == SIMPLE_DIFF_JSON
+
+
+def test_complex_json_diff():
+    difference = generate_diff(
+        PATH_BEFORE_COMPLEX,
+        PATH_AFTER_COMPLEX,
+        'json')
+    assert difference == COMPLEX_DIFF_JSON
+
+
+def test_simple_plain_diff():
+    difference = generate_diff(
+        PATH_BEFORE_SIMPLE,
+        PATH_AFTER_SIMPLE,
+        'plain')
+    assert difference == SIMPLE_DIFF_PLAIN
+
+
+def test_complex_plain_diff():
+    difference = generate_diff(
+        PATH_BEFORE_COMPLEX,
+        PATH_AFTER_COMPLEX,
+        'plain')
+    assert difference == COMPLEX_DIFF_PLAIN
